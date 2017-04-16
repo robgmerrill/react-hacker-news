a0e67d6c0de2016bbb1be068840f7d3bcb820361
robgmerrill <robgmerrill@gmail.com>
2017-04-16 10:43:02 -0700

    rendered a list with map function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+},
+];
+
+// ES6 class component with the name App - A component declaraction
+// When compnent is declared you can use it as an element everywhere in your application
+// It will produce and instance of your component or in other words: the component gets
+// instantiated.
 class App extends Component {
+  // element component returns is specified with render() method.
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { list.map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>Author: {item.author} </span>
+            <span>Comments: {item.num_comments} </span>
+            <span>Points: {item.points} </span>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// ReactDom.render() uses a DOM node in your HTML to replace it wit your JSX.
+// ReactDom.render() expects two arguments: JSX that gets rendered, second argument
+// specifies the place where the React application hooks into your HTML. It expects
+// an an element with an id='root'
 ReactDOM.render(
+  // You instantiate your "App" component with <App />
   <App />,
   document.getElementById('root')
 );
+
+if (module.hot) {
+  module.hot.accept()
+}
